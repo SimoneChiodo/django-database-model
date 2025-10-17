4840a648ea3860e426634a850c136784e313da71
SimoneChiodo <simochi.005@gmail.com>
2025-10-17 18:23:29 +0200

    Add postDetail page

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def home(request):
   posts = Post.objects.all().order_by('-data_creazione')
   return render(request, "index.html", { "posts": posts })
+
+def postDetail(request, post_id):
+  post = get_object_or_404(Post, id=post_id)
+  return render(request, "postDetail.html", {"post": post})
