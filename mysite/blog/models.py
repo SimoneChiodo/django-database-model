61b4134570f9f0fb8cc02aa1899d3f60c8afb07f
SimoneChiodo <simochi.005@gmail.com>
2025-10-17 18:00:57 +0200

    Add posts list to home page

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
   titolo = models.CharField(max_length=200)
-  contenuto = models.TextField()
+  descrizione = models.TextField()
   data_creazione = models.DateTimeField(auto_now_add=True)
 
   def __str__(self):
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def home(request):
-  return render(request, "index.html")
+  posts = Post.objects.all().order_by('-data_creazione')
+  return render(request, "index.html", { "posts": posts })
